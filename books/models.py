0bad704b3d86461d3019a9158b730b588020355a
roanblanchard <roan.g.blanchard@icloud.com>
2021-06-21 18:42:43 -0400

    admin, models, views, and urls are set up

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 
 class User(AbstractUser):
@@ -8,3 +9,11 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    url = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now) 
--- books/views.py
+++ books/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Book
+from django.utils import timezone
 
 # Create your views here.
+
+
+def book_list(request):
+    book = Book.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'books/book_list.html', {'book': book})
